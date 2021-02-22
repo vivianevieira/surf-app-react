428dfa34fa2462642de19613e291c94c7e6eb52d
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-21 19:15:32 -0800

    Added style and linke to header title.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Header() {
   return (
     <header>
-      <div>
-        Header
+      <div className="nav-bar">
+        <div className="nav-title">
+          <h3><a href="#">Surf Check</a></h3>
+        </div>
       </div>
     </header>
   )
