d4d11d20d7bb8aeb982cef746f5e03468cb86ecb
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-22 14:30:47 -0800

    Bottom nav component created from a tabs obj.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faHome, faUserCircle, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+const tabs = [{
+  // route: "/home",
+  icon: faHome,
+  label: "Home"
+},{
+  // route: "/login",
+  icon: faHeart,
+  label: "Favorites"
+}]
 
 export default function Footer() {
   return (
     <div className="bottom-nav">
       <div className="bottom-nav-container">
-        <div className="bottom-nav-tabs">First item</div>
-        <div className="bottom-nav-tabs">Second item</div>
+        {tabs.map((tab, index) => (
+        <div className="bottom-nav-tabs" key={`tab-${index}`}>
+          <FontAwesomeIcon size="lg" icon={tab.icon} />
+          <div>{tab.label}</div>
+        </div>
+        ))}
       </div>
     </div>
   )
