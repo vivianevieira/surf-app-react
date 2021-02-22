71d28b434ba52d4c46ff9e61c1c78656e427d2b7
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-22 13:06:32 -0800

    Started bottom-nav container.

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="bottom-nav">
+      <div className="bottom-nav-container">
+        <div className="bottom-nav-tabs">First item</div>
+        <div className="bottom-nav-tabs">Second item</div>
+      </div>
+    </div>
+  )
+};
