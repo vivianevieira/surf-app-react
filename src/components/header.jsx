a85b6417b749340ee7cd9a482b14c3aafad1c79f
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-01 10:27:37 -0800

    Completed Router Navlink on the bottom nav bar.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header>
       <div className="nav-bar">
         <div className="nav-title">
-          <h3><a href="#">Surf Check</a></h3>
+          <h3><Link to="/">Surf Check</Link></h3>
         </div>
       </div>
     </header>
