edd62e026d4179c3c9da6623911202a1b975d49c
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-01 09:44:55 -0800

    Setup BrowserRouter and imported view of Home and Favorites.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 import Header from './components/header';
 import Footer from './components/footer';
 
 function App() {
   return (
     <>
-    <Header />
-      <div>
-        <p>Hello World</p>
-      </div>
-     <Footer />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+     </BrowserRouter>
     </>
   );
 }
