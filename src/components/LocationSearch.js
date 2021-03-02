2ec211f032f6c73af0729b3e9c19d657ed1b2139
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-02 09:45:10 -0800

    Creatd a component for Location Search input field.

--- src/components/LocationSearch.js
+++ src/components/LocationSearch.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/components/LocationSearch.module.css';
 
 export default function LocationSearch() {
@@ -15,8 +17,13 @@ export default function LocationSearch() {
             placeholder="Search for surf spot or city"
            />
         </div>
+        <div className={styles.locationSearchBtn}>
+          <button type="submit" id="locationSearchBtn">
+            <FontAwesomeIcon icon={faCompass} className={styles.locationSearchBtnMargin} />
+            Search
+          </button>
+        </div>
       </form>
-      <div>Search Bar</div>
     </>
   );
 }
