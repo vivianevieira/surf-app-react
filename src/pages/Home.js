e09cdc54fa65eaa2568bb42d570b08bb8abf9fba
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-03 10:36:14 -0800

    Created a component to render the search results.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import LocationSearch from '../components/LocationSearch';
+import SearchResults from '../components/SearchResults';
 
 export default function Home() {
   return (
     <>
       <LocationSearch />
-      <div>Home</div>
+      <SearchResults />
     </>
   )
 };
