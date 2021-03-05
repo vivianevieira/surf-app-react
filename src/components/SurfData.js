e8774e5f081c69fb47ae5002f92a01bf2e875708
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-05 14:19:41 -0800

    Added data for Wind Swell.

--- src/components/SurfData.js
+++ src/components/SurfData.js
@@ -10,29 +10,43 @@ export default function SurfData({ location, loading, surfData }) {
 
   const {
     waveHeight,
-    wavePeriod,
-    waveDirection,
+    swellDirection,
+    swellPeriod,
     secondarySwellHeight,
     secondarySwellPeriod,
     secondarySwellDirection,
+    windWaveHeight,
+    windWaveDirection,
+    windWavePeriod,
     waterTemperature,
     airTemperature,
     windSpeed,
-    windDirection
+    windDirection,
+    gust
   } = surfData;
 
   console.log('loading', loading)
   console.log('location', location)
   console.log('surfData', surfData)
 
+  // get values and convert from mt to ft, from C° to F°, and from mt to knot
+
   const waveHeightValue = Math.round(waveHeight.noaa * 3.281);
-  const wavePeriodValue = wavePeriod.noaa;
-  const waveDirectionValue = waveDirection.noaa;
+  const swellDirectionValue =Math.round(swellDirection.noaa);
+  const swellPeriodValue = Math.round(swellPeriod.noaa);
   const secondarySwellHeightValue = Math.round(secondarySwellHeight.noaa * 3.281);
-  const secondarySwellPeriodValue = secondarySwellPeriod.noaa;
-  const secondarySwellDirectionValue = secondarySwellDirection.noaa;
+  const secondarySwellPeriodValue = Math.round(secondarySwellPeriod.noaa);
+  const secondarySwellDirectionValue = Math.round(secondarySwellDirection.noaa);
+  const windWaveHeightValue = Math.round(windWaveHeight.noaa * 3.281);
+  const windWavePeriodValue = Math.round(windWavePeriod.noaa);
+  const windWaveDirectionValue = Math.round(windWaveDirection.noaa);
 
+  const waterTemperatureValue = Math.round(waterTemperature.noaa * 9 / 5) + 32;
+  const airTemperatureValue = Math.round(airTemperature.noaa * 9 / 5) + 32;
 
+  const windSpeedValue = Math.round(windSpeed.noaa * 1.944);
+  const windDirectionValue = Math.round(windDirection.noaa);
+  const gustValue = Math.round(gust.noaa * 1.944);
 
   return (
     <>
@@ -53,10 +67,14 @@ export default function SurfData({ location, loading, surfData }) {
         <div>
           <div className={styles.SurfDataWaveHeightTitle}>Wave Height</div>
           <div className={styles.SurfDataWaveHeightvalue}>{waveHeightValue}ft</div>
-          <div className={styles.SurfDataPrimarySwell}>{wavePeriodValue}s {waveDirectionValue}°</div>
+          <div className={styles.SurfDataPrimarySwell}>{swellPeriodValue}s {swellDirectionValue}°</div>
           <div className={styles.SurfDataPrimarySwellTitle}>Secondary Swell</div>
           <div>
             {`${secondarySwellHeightValue}ft at ${secondarySwellPeriodValue}s ${secondarySwellDirectionValue}°`}
+          </div>
+          <div className={styles.SurfDataPrimarySwellTitle}>Wind Swell</div>
+          <div>
+            {`${windWaveHeightValue}ft at ${windWavePeriodValue}s ${windWaveDirectionValue}°`}
           </div>
         </div>
         <div>
@@ -65,19 +83,19 @@ export default function SurfData({ location, loading, surfData }) {
               <div>
                 <FontAwesomeIcon icon={faWater} size="lg" className={styles.SurfDataWaterTempIcon} />
               </div>
-              <div>58°F</div>
+              <div>{`${waterTemperatureValue}°F`}</div>
             </div>
             <div className={styles.SurfDataAirTemp}>
               <div>
                 <FontAwesomeIcon icon={faThermometerHalf} size="lg" className={styles.SurfDataAirTempIcon} />
               </div>
-              <div>57°F</div>
+              <div>{`${airTemperatureValue}°F`}</div>
             </div>
           </div>
           <div className={styles.SurfDataWindCont}>
             <div className={styles.SurfDataWindTitle}>Wind</div>
-            <div>13kts 164 °</div>
-            <div className={styles.SurfDataWindGust}>(16kts gusts)</div>
+            <div>{`${windSpeedValue}kts ${windDirectionValue}°`}</div>
+            <div className={styles.SurfDataWindGust}>{`(${gustValue}kts gusts)`}</div>
           </div>
         </div>
       </div>
