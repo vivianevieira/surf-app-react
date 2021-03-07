3896591bfdecdf972f7371d35e7e46a77d6344e4
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-06 20:18:23 -0800

    Styles component.

--- src/components/SunlightTimes.js
+++ src/components/SunlightTimes.js
@@ -2,21 +2,32 @@
 import styles from '../styles/components/SunlightTimes.module.css';
 
 export default function SunlightTimes() {
+  const dateNow = new Date();
+  const options = { month: 'long' };
+  const year = dateNow.getFullYear();
+  const day = dateNow.getDate();
+  const month = new Intl.DateTimeFormat('en-US', options).format(dateNow);
+  const dateHeader = `${month} ${day}, ${year}`;
+
   return (
     <div className={styles.SunlightTimesCont}>
-      <div className={styles.SunlightTimesDateHeader}>March 6, 2021</div>
+      <div className={styles.SunlightTimesDateHeader}>{dateHeader}</div>
       <div className={styles.SunlightTimesSunrise}>
-        <div className={styles.SunlightTimesIcon}>Icon</div>
+        <div className={styles.SunlightTimesIcon}>
+          <img src="../images/sunrise.png" alt="" width="35px" />
+        </div>
         <div>
-          <div>First light:</div>
-          <div>Sunrise:</div>
+          <div>First light: <span className={styles.SunlightTimesValue}>5:45 am</span></div>
+          <div>Sunrise: <span className={styles.SunlightTimesValue}>6:10 am</span></div>
         </div>
       </div>
       <div className={styles.SunlightTimesSunrise}>
-        <div className={styles.SunlightTimesIcon}>Icon</div>
+        <div className={styles.SunlightTimesIcon}>
+          <img src="../images/sunset.png" alt="" width="35px" />
+        </div>
         <div>
-          <div>Sunset:</div>
-          <div>Last light:</div>
+          <div>Sunset: <span className={styles.SunlightTimesValue}>5:52 pm</span></div>
+          <div>Last light: <span className={styles.SunlightTimesValue}>6:16 pm</span></div>
         </div>
       </div>
     </div>
