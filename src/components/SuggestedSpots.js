2bebca2a43b136b1fa0474c7e84a4eef1fa40996
Viviane Vieira <viviane.kodama@gmail.com>
2021-03-15 15:57:29 -0700

    Created an onClick function for suggested surf spots that fetches data from OpenCage API.

--- src/components/SuggestedSpots.js
+++ src/components/SuggestedSpots.js
@@ -1,27 +1,117 @@
+import { useContext, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { SurfDataContext } from '../contexts/SurfDataContext';
 import styles from '../styles/components/SuggestedSpots.module.css';
 
+const locationsList = [
+  {
+    formatted: 'Jeffreys Bay, South Africa',
+    geometry: {
+      lat: -34.0297919,
+      lng: 24.8960619
+    },
+    annotations: {
+      timezone: {
+        offset_sec: 7200
+      }
+    },
+    countryFlag: 'za'
+  },
+  {
+    formatted: 'Sunset Beach, Hawaii USA',
+    geometry: {
+      lat: 21.6707387,
+      lng: -158.0455857
+    },
+    annotations: {
+      timezone: {
+        offset_sec: -36000
+      }
+    },
+    countryFlag: 'us'
+  },
+  {
+    formatted: 'Gold Coast, Queensland, Australia',
+    geometry: {
+      lat: -28.0023731,
+      lng: 153.4145987
+    },
+    annotations: {
+      timezone: {
+        offset_sec: 36000
+      }
+    },
+    countryFlag: 'au'
+  },
+    {
+    formatted: 'Trestles Beach, CA USA',
+    geometry: {
+      lat: 33.5111848,
+      lng: -117.6849323
+    },
+    annotations: {
+      timezone: {
+        offset_sec: -28800
+      }
+    },
+    countryFlag: 'us'
+  },
+   {
+    formatted: 'Swamis Beach, CA USA',
+    geometry: {
+      lat: 33.0466108,
+      lng: -117.2563856
+    },
+    annotations: {
+      timezone: {
+        offset_sec: -28800
+      }
+    },
+    countryFlag: 'us'
+  }
+
+]
+
 export default function SuggestedSpots() {
+  const { handleLocationClicked } = useContext(SurfDataContext);
+
+  const [location, setLocation] = useState({});
+
+  const handleSuggestedSpotClicked = async (props) => {
+    const { formatted, geometry } = props;
+
+    const lat = geometry.lat;
+    const lng = geometry.lng;
+
+    const url = 'https://api.opencagedata.com/geocode/v1/json';
+    const apiKey = '2338c18f4b274400b9a2969d91cba7c7';
+
+    try {
+      const searchUrl = `${url}?q=${lat}+${lng}&key=${apiKey}`;
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+      console.log('data', data.results[0]);
+      const locationData =  data.results[0];
+      setLocation({ ...locationData, formatted: formatted});
+      console.log(location);
+    } catch (e) {
+      console.log(e);
+    }
+
+  }
+
   return (
     <div className={styles.SuggestedSpotsContainer}>
+      <p>Suggestions of famous surf spots:</p>
       <ul>
-        <li data-long="24.8960619" data-lat="-34.0297919" data-fullname="Jeffrey's Bay, South Africa" data-utcoffset="7200">
-          <p>Suggestions of famous surf spots:</p>
-          <br />
-          Jeffreys Bay, South Africa
-        </li>
-        <li data-long="-158.0455857" data-lat="21.6707387" data-fullname="Sunset Beach, Hawaii USA" data-utcoffset="-36000">
-          <img src="https://www.countryflags.io/us/flat/16.png" />
-          Sunset Beach, Hawaii USA
-        </li>
-        <li data-long="153.4145987" data-lat="-28.0023731" data-fullname="Gold Coast, Queensland, Australia" data-utcoffset="36000">
-          Gold Coast, Queensland, Australia
-        </li>
-        <li data-long="-117.6849323" data-lat="33.5111848" data-fullname="Trestles Beach, CA USA" data-utcoffset="-28800">
-          Trestles Beach, CA USA
-        </li>
-        <li data-long="-117.2563856" data-lat="33.0466108" data-fullname="Swamis Beach, CA USA" data-utcoffset="-28800">
-          Swamis Beach, CA USA
-        </li>
+        {locationsList.map((loc, index) => (
+          <li key={`loc-${index}`}>
+            <NavLink to="/" onClick={() => handleSuggestedSpotClicked(loc)}>
+              <img className={styles.SuggestedSpotsFlag} src={`https://www.countryflags.io/${loc.countryFlag}/flat/24.png`} />
+              {loc.formatted}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </div>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
     <>
       <LocationSearch />
       <SuggestedSpots />
+      <SearchResults />
     </>
   )
 };
